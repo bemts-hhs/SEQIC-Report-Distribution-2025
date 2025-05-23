5c7e00b923b555dafaa0d4079d5cc61d29c7310d
Bureau of Emergency Medical and Trauma Services <nicolas.foss@hhs.iowa.gov>
2025-05-23 08:52:18 -0500

    set up the project and repo

---
+++ R/seqic_data_load.R
@@ -0,0 +1,27 @@
+###_____________________________________________________________________________
+### Loading data for the distribution of SEQIC reports in 2025
+### All data for these reports will be 2020 - 2024 with 2024 being the 'current'
+### year
+###_____________________________________________________________________________
+
+# load files from source
+trauma_2020 <-
+  readr::read_csv(
+    "C:/Users/nfoss0/OneDrive - State of Iowa HHS/Analytics/BEMTS/TRAUMA DATA/SEQIC/trauma_2020.csv"
+  )
+trauma_2021 <-
+  readr::read_csv(
+    "C:/Users/nfoss0/OneDrive - State of Iowa HHS/Analytics/BEMTS/TRAUMA DATA/SEQIC/trauma_2021.csv"
+  )
+trauma_2022 <-
+  readr::read_csv(
+    "C:/Users/nfoss0/OneDrive - State of Iowa HHS/Analytics/BEMTS/TRAUMA DATA/SEQIC/trauma_2022.csv"
+  )
+trauma_2023 <-
+  readr::read_csv(
+    "C:/Users/nfoss0/OneDrive - State of Iowa HHS/Analytics/BEMTS/TRAUMA DATA/SEQIC/trauma_2023.csv"
+  )
+trauma_2024 <-
+  readr::read_csv(
+    "C:/Users/nfoss0/OneDrive - State of Iowa HHS/Analytics/BEMTS/TRAUMA DATA/SEQIC/trauma_2024.csv"
+  )
